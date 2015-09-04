1d55e50dd12b7c56543cd9f482a8b108c5976c95
Andrei Balan <andrei@Andreis-MacBook-Pro-2.local>
2015-09-04 12:57:40 +0300

    Casino 1.0

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,10 +14,89 @@ var app = angular.module('emJsAcademy', [
         console.error('$stateChangeError', event, toState, toParams, fromState, fromParams)
     });
 
-    emWebApi.call('/user/account#getCountries').then(function (list) {
+/*    emWebApi.call('/user/account#getCountries').then(function (list) {
         console.log('here are the language list', list)
-    }, function(err){
+    },
+    emWebApi.call('/casino#getGames', {expectedFields: 4 + 16}).then(function (list){
+        console.log('here is the game list', list);
+    }), function(err){
         console.log('err', err)
-    })
+    })*/
+
 });
 window.global = {};
+
+/*----------------------------------------Service------------------------------------------------------*/
+
+app.factory('requestGames', ['emWebApi', function(emWebApi) {
+    var parameters = { expectedFields: 1 + 2 + 4 + 8 + 16 + 32 + 65536 + 8192 + 33554432 };
+    return {
+        getGames: function () {
+            return emWebApi.call('/casino#getGames', parameters).then(function (list){
+                return list;
+            })
+        }
+    }
+}])
+
+app.factory('requestVendors', ['emWebApi', function(emWebApi) {
+    return {
+        getVendors: function () {
+            return emWebApi.call('/casino#getGameVendors').then(function (vendorList){
+                return vendorList;
+            })
+        }
+    }
+}])
+
+app.factory('requestCategories', ['emWebApi', function(emWebApi) {
+    return {
+        getCategories: function () {
+            return emWebApi.call('/casino#getGameCategories').then(function (categList){
+                return categList;
+            })
+        }
+    }
+}])
+
+/*--------------------------------------Controller------------------------------------------------------*/
+
+app.controller('MainCtrl', ['$scope', 'requestGames', 'requestVendors', 'requestCategories', function($scope, requestGames, requestVendors, requestCategories){
+    $scope.gamesList = [];
+    $scope.vendorList = [];
+    requestGames.getGames().then(function(data){
+        $scope.gamesList = data;
+        console.log('first game name', data.games[0]);
+        console.log('scope gamelist', $scope.gamesList);
+        $scope.$apply();
+  
+    });
+    requestVendors.getVendors().then(function(data) {
+        $scope.vendorList = data.vendors;
+        $scope.$apply();
+        console.log('vendor list', $scope.vendorList);
+
+    });
+    requestCategories.getCategories().then(function(data) {
+        $scope.categList = data.categories;
+        $scope.$apply();
+        console.log('categ list', $scope.categList);
+
+    });
+}]);
+
+/*--------------------------------------Directive-------------------------------------------------------*/
+
+app.directive('displayGame', ['requestGames', function() {
+    return {
+        restrict: 'E',
+        templateUrl: require('./components/games.html')
+    }
+}]);
+
+app.directive('displayCategory', ['requestCategories', function() {
+    return {
+        restrict: 'E',
+        templateUrl: require('./components/categories.html')
+    }
+}])
